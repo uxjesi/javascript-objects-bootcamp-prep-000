39107b334dd9773c40291fff20dd2a5bd0748a33
Jesi Loredo <jesi.thegreat@gmail.com>
2018-07-13 02:10:42 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,8 +2,8 @@ var playlist = {Slowdive: "Alison"}
 
 function updatePlaylist (obj, key, pair ) {
       
-    playlist.Slowdive = 'Alison'      
-    playlist.MyBloodyValentine = 'Sometimes'    
+    playlist['My Bloody Valentine'] = 'Sometimes'      
+    playlist['Phil Ochs'] = 'Here/s to the State of Mississippi'    
       return playlist
   
 }
